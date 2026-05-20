639a32d130701c11f8bc1eb4896831783fbbb529
TharunRocky <58210625+TharunRocky@users.noreply.github.com>
2026-05-20 20:46:11 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
       setLoading(true);
 
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://mynotes-9754.onrender.com/api/auth/login",
         form
       );
 
